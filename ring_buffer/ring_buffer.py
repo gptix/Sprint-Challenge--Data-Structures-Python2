d7dc0a4c1668f7a68d126d5fa823ab39efa77cfa
Jud Taylor <jud.taylor@protonmail.com>
2020-06-12 11:27:13 -0400

    Passed all tests.

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,17 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
-
-    def append(self, item):
-        pass
-
+        self.storage = [None]*capacity
+        self.write_pointer = 0
+        self.capacity = capacity
+        
+    def append(self, val):
+        self.storage[self.write_pointer] = val
+        self.write_pointer = (self.write_pointer + 1) % self.capacity
+        
     def get(self):
-        pass
+        return [el for el in self.storage if el is not None]
+
+# rb = RingBuffer(5)
+# rb.append('a')
+# foo  = rb.get()
+# print(foo)                
